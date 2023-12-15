4e30bcb3d6295a2cbf95bd962fd26f3c3a111630
Yuvraj Rathi <yrathi66@gmail.com>
2023-12-16 01:10:49 +0530

    Add eveything till navbar

---
+++ lib/elixir_gist/gists/gist.ex
@@ -0,0 +1,24 @@
+defmodule ElixirGist.Gists.Gist do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "gists" do
+    field :description, :string
+    field :markup_text, :string
+    field :name, :string
+    belongs_to :user, ElixirGist.Accounts.User
+    has_many :comments, ElixirGist.Comments.Comment
+
+
+    timestamps(type: :utc_datetime)
+  end
+
+  @doc false
+  def changeset(gist, attrs) do
+    gist
+    |> cast(attrs, [:name, :description, :markup_text, :user_id])
+    |> validate_required([:name, :description, :markup_text, :user_id])
+  end
+end
---
+++ test/support/fixtures/gists_fixtures.ex
@@ -0,0 +1,36 @@
+defmodule ElixirGist.GistsFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `ElixirGist.Gists` context.
+  """
+
+  @doc """
+  Generate a gist.
+  """
+  def gist_fixture(attrs \\ %{}) do
+    {:ok, gist} =
+      attrs
+      |> Enum.into(%{
+        description: "some description",
+        markup_text: "some markup_text",
+        name: "some name"
+      })
+      |> ElixirGist.Gists.create_gist()
+
+    gist
+  end
+
+  @doc """
+  Generate a saved_gist.
+  """
+  def saved_gist_fixture(attrs \\ %{}) do
+    {:ok, saved_gist} =
+      attrs
+      |> Enum.into(%{
+
+      })
+      |> ElixirGist.Gists.create_saved_gist()
+
+    saved_gist
+  end
+end

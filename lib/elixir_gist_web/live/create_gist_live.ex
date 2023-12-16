9480b08364d06a3b965a2e9756b88947b62dcb70
Yuvraj Rathi <yrathi66@gmail.com>
2023-12-16 16:50:24 +0530

    Add footer, gradient + Routed a LiveView

---
+++ lib/elixir_gist_web/live/create_gist_live.ex
@@ -0,0 +1,12 @@
+defmodule ElixirGistWeb.CreateGistLive do
+  use ElixirGistWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+  # def render(assigns) do
+  #   ~H"""
+
+  #   """
+  # end
+end
